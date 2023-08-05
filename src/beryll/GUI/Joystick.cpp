e73243bdd4733f5b84898b275d86aef34cbef6ee
Uladzimir Karol <you@example.com>
2023-08-05 19:55:04 +0200

    Update Joystick.

--- src/beryll/GUI/Joystick.cpp
+++ src/beryll/GUI/Joystick.cpp
@@ -22,20 +22,12 @@ namespace Beryll
             m_touchedTexture = Renderer::createTexture(touchedTexturePath.c_str(), TextureType::DIFFUSE_TEXTURE);
 
         float leftPosPixels = m_leftPos * MainImGUI::getInstance()->getGUIWidth();
-        if(leftPosPixels < 0.0f)
-            leftPosPixels = 0.0f;
         float rightPosPixels = leftPosPixels + (MainImGUI::getInstance()->getGUIWidth() * m_width);
         m_joystickOrigin.x = leftPosPixels + ((rightPosPixels - leftPosPixels) * 0.5f);
-        //BR_INFO("Joystick leftPos:%f rightPos:%f", leftPosPixels, rightPosPixels);
-        //BR_INFO("m_joystickOrigin.x:%f", m_joystickOrigin.x);
 
         float topPosPixels = m_topPos * MainImGUI::getInstance()->getGUIHeight();
-        if(topPosPixels < 0.0f)
-            topPosPixels = 0.0f;
         float bottomPosPixels = topPosPixels + (m_height * MainImGUI::getInstance()->getGUIHeight());
         m_joystickOrigin.y = topPosPixels + ((bottomPosPixels - topPosPixels) * 0.5f);
-        //BR_INFO("Joystick topPosPixels:%f bottomPosPixels:%f", topPosPixels, bottomPosPixels);
-        //BR_INFO("m_joystickOrigin.y:%f", m_joystickOrigin.y);
     }
 
     Joystick::~Joystick()
@@ -55,33 +47,14 @@ namespace Beryll
             if(f.normalizedPos.x > m_leftPos && f.normalizedPos.x < m_leftPos + m_width
                && f.normalizedPos.y > m_topPos && f.normalizedPos.y < m_topPos + m_height)
             {
-                // if any finger in joystick area
+                // If any finger in joystick area.
                 m_touched = true;
 
-                //BR_INFO("f.ImGuiScreenPos X: %f Y: %f", f.ImGuiScreenPos.x, f.ImGuiScreenPos.y);
+                if(f.downEvent && !f.handled)
+                    f.handled = true;
 
                 m_touchedDirectionFromOrigin = glm::normalize(f.ImGuiScreenPos - m_joystickOrigin);
                 m_touchedDirectionFromOrigin.y = -m_touchedDirectionFromOrigin.y;
-                m_touchedDirectionFromOrigin = glm::normalize(m_touchedDirectionFromOrigin);
-
-                //BR_INFO("Joystick dir X: %f Y: %f", m_touchedDirectionFromOrigin.x, m_touchedDirectionFromOrigin.y);
-
-                float touchAngle = Utils::Common::getAngleInDegrees(m_joystickUp, m_touchedDirectionFromOrigin);
-                if(touchAngle < 15.0f)
-                {
-                    // Move directly to front.
-                    m_touchedDirectionFromOrigin = glm::vec2{0.0f, 1.0f};
-                }
-                else if(touchAngle > 165.0f)
-                {
-                    // Move directly to back.
-                    m_touchedDirectionFromOrigin = glm::vec2{0.0f, -1.0f};
-                }
-
-                if(f.downEvent && !f.handled)
-                {
-                    f.handled = true;
-                }
             }
         }
     }
